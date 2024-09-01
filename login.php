4f296a3a09f647edee2e0cdaebd23013ea5e16eb
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2024-09-01 22:06:54 +0300

    adding php

--- login.php
+++ login.php
@@ -1,4 +1,32 @@
 <?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+// Connect to DB
+    $conn = mysqli_connect("localhost", "root", "", "LMS");
+    if (!$conn) {
+        mysqli_connect_error();
+        exit;
+    }
+// Escape any special character to avoid sql injection
+    $email = mysqli_escape_string($conn, $_POST['email']);
+    $password = sha1($_POST['password']);
+
+    $query = "SELECT * FROM `users` WHERE `email` = '" . $email . "' AND password = '" . $password . "' LIMIT 1";
+    $result = mysqli_query($conn, $query);
+
+    if ($row = mysqli_fetch_assoc($result)) {
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['email'] = $row['email'];
+        header("Location: profile.php");
+        exit;
+    } else {
+        $error = "Invalid email or password";
+    }
+    // Close the connection
+    mysqli_free_result($result);
+    mysqli_close($conn);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,14 +43,18 @@
     <form action="" method="POST">
         <div class="input-group">
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" required/>
+            <input type="email" id="email" name="email" value="<?= $_POST['email'] ?? "" ?>" required/>
         </div>
         <div class="input-group">
             <label for="password">Password</label>
             <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <?php if (isset($error)) echo $error; ?>
         </div>
         <button type="submit">Login</button>
     </form>
 </div>
 </body>
 </html>
+
